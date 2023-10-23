50ad5e0d165f3e4447a8a033f5c65ac93f1c4488
GNANESH-G <gnanesh.gnanapazhani@kumaran.com>
2023-10-23 17:04:06 +0530

    Final Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { FormComponent } from './components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomePageComponent},
+  {path:'form', component:FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { FormsModule } from '@angular/forms';
 import { FormService } from './services/form.service';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent],
+  declarations: [AppComponent, FormComponent, HomePageComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [FormService],
   bootstrap: [AppComponent],
